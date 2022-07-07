4c0b629d8a44c0ce6b09474bb194150a670404e5
Nick-Tran1501 <68211831+Nick-Tran1501@users.noreply.github.com>
2022-07-07 17:02:31 +0700

    Add React Folder

---
+++ conflict/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const app = () => {
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae, perferendis?</div>
+  )
+}
+
+export default app
---
+++ conflict/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+
+const index = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
